f0fecc0572820907214280a3a49a50f23a29e731
YuexLi <695426057@qq.com>
2018-03-29 15:21:29 +0800

    feat (viewpoint): 添加top,header组件，并配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    Hello world
+    <m-header></m-header>
+    <m-tab></m-tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import MHeader from 'components/m-header/m-header'
+import MTab from 'components/tab/tab'
 export default {
+  components: {
+    MHeader,
+    MTab
+  },
   name: 'App'
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus" type="text/stylus">
-  @import "common/stylus/variable"
+  @import "~common/stylus/variable"
 
   #app
     color: $color-theme
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="m-header">
+  <div class="icon"></div>
+  <h1 class="text">Chicken Music</h1>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+}
+</script>
+
+<style scoped type="text/stylus" lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable";
+  @import "~common/stylus/mixin";
+
+  .m-header
+    position: relative
+    height: 40px
+    text-align: center
+    color: $color-theme
+    font-size: 0
+    .icon
+      display: inline-block
+      vertical-align top
+      margin_top: 6px
+      width: 30px
+      height: 32px
+      margin-right: 9px
+      bg-image('logo')
+      background-size: 30px 32px
+    .text
+      display: inline-block
+      vertical-align: top
+      line-height: 40px
+      font-size: $font-size-large
+</style>
