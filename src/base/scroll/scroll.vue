1d46587ea5c19d2ebd20810f9e685ef963144966
YuexLi <695426057@qq.com>
2018-03-31 22:45:38 +0800

    feat(view-layer): 添加歌手页面滚动，以及快速切换
    1. 添加页面滚动
    2. 右边快速支持点击
    3. 快速切换支持拖动
    4. 左右支持联动

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -22,6 +22,10 @@ export default {
     data: {
       type: Array,
       default: null
+    },
+    listenScroll: {
+      type: Boolean,
+      default: false
     }
   },
   mounted () {
@@ -38,6 +42,15 @@ export default {
         probeType: this.probeType,
         click: this.click
       })
+
+      if (this.listenScroll) {
+        let me = this
+        // 监听滚动事件，并向外发送scroll事件
+        // pos是对象，包含x,y的属性
+        this.scroll.on('scroll', (pos) => {
+          me.$emit('scroll', pos)
+        })
+      }
     },
     enable () {
       this.scroll && this.scroll.enable()
@@ -47,6 +60,12 @@ export default {
     },
     refresh () {
       this.scroll && this.scroll.refresh()
+    },
+    scrollTo () {
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+    },
+    scrollToElement () {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
     }
   },
   watch: {
