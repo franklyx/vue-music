de04c12b023dc11f6c55f5c193620b6ab6c3d1d1
YuexLi <695426057@qq.com>
2018-04-15 17:37:02 +0800

    feat(view-layer): 将js添加为异步懒加载功能，优化页面响应

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,50 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Reacommend from 'components/recommend/recommend'
-import Singer from 'components/singer/singer'
-import Rank from 'components/rank/rank'
-import Search from 'components/search/search'
-import SingerDetail from 'components/singer-detail/singer-detail'
-import Disc from 'components/disc/disc'
-import TopList from 'components/top-list/top-list'
 
 Vue.use(Router)
+
+// 添加懒加载代码
+const Recommend = (resolve) => {
+  import('components/recommend/recommend').then((module) => {
+    resolve(module)
+  })
+}
+
+const Singer = (resolve) => {
+  import('components/singer/singer').then((module) => {
+    resolve(module)
+  })
+}
+
+const Rank = (resolve) => {
+  import('components/rank/rank').then((module) => {
+    resolve(module)
+  })
+}
+
+const Search = (resolve) => {
+  import('components/search/search').then((module) => {
+    resolve(module)
+  })
+}
+
+const SingerDetail = (resolve) => {
+  import('components/singer-detail/singer-detail').then((module) => {
+    resolve(module)
+  })
+}
+
+const TopList = (resolve) => {
+  import('components/top-list/top-list').then((module) => {
+    resolve(module)
+  })
+}
+
+const Disc = (resolve) => {
+  import('components/disc/disc').then((module) => {
+    resolve(module)
+  })
+}
 
 export default new Router({
   mode: 'history',
@@ -19,7 +55,7 @@ export default new Router({
     },
     {
       path: '/recommend',
-      component: Reacommend,
+      component: Recommend,
       children: [
         {
           path: ':id',
